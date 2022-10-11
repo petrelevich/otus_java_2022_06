5b788ce5df9feb670b0c5a61bc21e2a228909b07
sergey <petrelevich@yandex.ru>
2022-10-11 09:44:36 +0300

    L28 websockets

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -18,4 +18,14 @@ public interface Versions {
 
     String reflections = "0.10.2";
 
+    String sockjs = "1.5.1";
+    String stomp = "2.3.4";
+    String bootstrap = "5.1.3";
+    String springDocOpenapiUi = "1.6.7";
+    String jsr305 = "3.0.2";
+    String grpc = "1.44.1";
+    String errorProneAnnotations = "2.11.0";
+    String tomcatAnnotationsApi = "6.0.53";
+
+    String kafkaClients = "3.0.0";
 }
