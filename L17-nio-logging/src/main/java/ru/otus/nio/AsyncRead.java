16dc3c97c0f7143a3652185cd1bcf404def592b0
sergey <petrelevich@yandex.ru>
2022-11-30 12:08:26 +0300

    L17 updated example

--- L17-nio-logging/src/main/java/ru/otus/nio/AsyncRead.java
+++ L17-nio-logging/src/main/java/ru/otus/nio/AsyncRead.java
@@ -6,16 +6,25 @@ import java.nio.channels.AsynchronousFileChannel;
 import java.nio.channels.CompletionHandler;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
-import java.util.concurrent.TimeUnit;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /*
 https://github.com/gridgain/gridgain/blob/d6222c6d892eabcbcfc60fd75fc2d38a7dd06bb6/modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/file/AsyncFileIO.java
  */
 
 public class AsyncRead implements AutoCloseable {
+    private static final Logger logger = LoggerFactory.getLogger(AsyncRead.class);
     private final ByteBuffer buffer = ByteBuffer.allocate(2);
     private final AsynchronousFileChannel fileChannel;
-    private final StringBuilder fileContent = new StringBuilder();
+    private final List<String> fileParts = new CopyOnWriteArrayList<>();
+    private final CountDownLatch latch = new CountDownLatch(1);
 
     private final CompletionHandler<Integer, ByteBuffer> completionHandler =
             new CompletionHandler<>() {
@@ -23,49 +32,50 @@ public class AsyncRead implements AutoCloseable {
 
                 @Override
                 public void completed(Integer readBytes, ByteBuffer attachment) {
-                    System.out.println(Thread.currentThread().getName() + ". readBytes:" + readBytes);
-                    byte[] destArray;
+                    logger.info("readBytes:{}", readBytes);
                     if (readBytes > 0) {
-                        destArray = new byte[readBytes];
+                        byte[] destArray = new byte[readBytes];
                         attachment.flip();
                         attachment.get(destArray, 0, destArray.length);
 
-                        lastPosition += readBytes;
-                        read(fileChannel, lastPosition, buffer);
-                        fileContent.append(new String(destArray));
+                        fileParts.add(new String(destArray));
+
+                        buffer.clear();
+                        var position = lastPosition += readBytes;
+                        fileChannel.read(buffer, position, buffer, completionHandler);
                     } else {
-                        System.out.println(Thread.currentThread().getName() + " fileData:\n" + fileContent);
+                        logger.info("read completed");
+                        latch.countDown();
                     }
                 }
 
                 @Override
                 public void failed(Throwable exc, ByteBuffer attachment) {
-                    System.err.println("error:" + exc.getMessage());
-                }
-
-                private void read(AsynchronousFileChannel fileChannel, int position, ByteBuffer buffer) {
-                    buffer.clear();
-                    fileChannel.read(buffer, position, buffer, completionHandler);
+                    logger.error("error:{}", exc.getMessage());
                 }
             };
 
 
     public static void main(String[] args) throws Exception {
-        try (var asyncRead = new AsyncRead()) {
+        var executor = Executors.newSingleThreadExecutor();
+        try (var asyncRead = new AsyncRead(executor)) {
             asyncRead.read();
+            executor.shutdown();
         }
     }
 
-    public AsyncRead() throws IOException {
-        fileChannel = AsynchronousFileChannel.open(Path.of("textFile.txt"), StandardOpenOption.READ);
+    public AsyncRead(ExecutorService executor) throws IOException {
+        fileChannel = AsynchronousFileChannel.open(Path.of("textFile.txt"),
+                Set.of(StandardOpenOption.READ), executor);
     }
 
     private void read() throws InterruptedException {
         fileChannel.read(buffer, 0, buffer, completionHandler);
+        logger.info("Hello");
+        latch.await();
 
-        System.out.println(Thread.currentThread().getName() + " Hello");
-
-        Thread.sleep(TimeUnit.SECONDS.toMillis(1));
+        var fileContent = String.join("", fileParts);
+        logger.info("fileContent:\n{}", fileContent);
     }
 
     @Override
